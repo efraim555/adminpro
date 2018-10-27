5e1af08aa2bc9b3e7d4c5a69f1037e6beab724ea
Efraim <regalo3xd@gmail.com>
2018-10-27 00:36:16 -0500

    Ruteo en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,20 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
-import {ProgressComponent} from './pages/progress/progress.component';
-import {Graficas1Component} from './pages/graficas1/graficas1.component';
 import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
-import {PagesComponent} from './pages/pages.component';
 import {RegisterComponent} from './login/register.component';
+import {PagesRoutingModule} from './pages/pages-routing.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'graficas1', component: Graficas1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
+const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', component: NopagefoundComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes, {useHash: true}), PagesRoutingModule],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 }
